97c4af3fce826d56123d1ab7ad470b362ab62249
Lisa Vu <lisavu125@gmail.com>
2017-11-18 14:18:41 -0500

    Java screen shot functionality

---
+++ src/TakeScreenshot.java
@@ -0,0 +1,38 @@
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.imageio.ImageIO;
+
+/*
+ * Program copied and modified from
+ * http://www.javatechblog.com/java/how-to-take-screenshot-programmatically-in-java/
+ */
+public class TakeScreenshot {
+
+  public static void main(String[] args) {
+    try {
+      Robot robot = new Robot();
+      String fileName = "E:\\" + timestamp() + ".png";
+
+      Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+      BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
+      ImageIO.write(screenFullImage, "png", new File(fileName));
+
+      System.out.println("Success!");
+
+    } catch (AWTException | IOException ex) {
+      System.err.println(ex);
+    }
+  }
+
+  private static String timestamp() {
+    return new SimpleDateFormat("yyyyMMdd HH-mm-ss").format(new Date());
+  }
+
+}
