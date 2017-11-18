aeeb4ba64dfb6fb1610a1d1caa9e7636778b6782
Sherry Mei <sherrymei95@gmail.com>
2017-11-18 18:46:40 -0500

    Added shell script

---
+++ test.sh
@@ -0,0 +1,35 @@
+run_git(){
+
+projDir=~/Desktop/
+artchives="ARTchive"
+git_file=".git"
+
+cd $projDir
+
+if [ ! -d "$projDir" ]; then
+	mkdir $artchives
+else
+	cd $artchives
+	if [ ! -e "$git_file" ]; then
+		git init
+		echo "file not found"
+	fi
+fi
+
+#when there is changes , git add and git commit 
+
+if git diff-index --quiet HEAD --; then
+    # no changes
+	return 0
+else
+    git add *
+    now=$(date +"%m-%d-%Y %r")
+    commitmessage="version_$now"
+    echo $commitmessage
+    git commit -m "$commitmessage"
+    return 1
+fi
+
+}
+
+run_git
