b56caf5c7c63e216a500d7023460f0dcab00e678
Lisa Vu <lisavu125@gmail.com>
2017-11-19 11:47:19 -0500

    Shell file

--- test.sh
+++ test.sh
@@ -10,12 +10,16 @@ else
 		git init
 		git commit -m "initital commit" --allow -empty
 	fi
-
-	git add *
-	now=$(date +"%m-%d-%Y %r")
-	commitmessage="version_$now"
+	if git diff-index --quiet HEAD --; then
+   # no changes	return 0
+		return 0;
+	else
+		git add *
+		now=$(date +"%m-%d-%Y %r")
+		commitmessage="version_$now"
 	  git commit -m "$commitmessage"
-	return 0
+		return 0
+	fi	
 fi
 
 #when there is changes , git add and git commit
